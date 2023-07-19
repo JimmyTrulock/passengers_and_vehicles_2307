fa0db47ccfeeb1dcce1fa7febf8f33d1d88b2d76
Jimmy Trulock <ttrulock23@gmail.com>
2023-07-19 10:35:27 -0600

    add entering and list_passengers method and testing

--- lib/park.rb
+++ lib/park.rb
@@ -1,9 +1,26 @@
+require './lib/vehicle'
+require './lib/passenger'
 
 class Park
-    attr_accessor :name, :admission_price
+    attr_accessor :name, :admission_price, :vehicles_entered
 
     def initialize(name, admission_price)
         @name = name
         @admission_price = admission_price
+        @vehicles_entered = []
+    end
+
+    def entering(vehicle)
+        @vehicles_entered << vehicle
+    end
+
+    def list_passengers
+        passenger_list = []
+        @vehicles_entered.each do |vehicle|
+            vehicle.passengers.each do |passenger|
+                passenger_list << passenger.name
+            end
+        end
+        passenger_list.join(", ")
     end
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -1,4 +1,6 @@
 require './lib/park'
+require './lib/vehicle'
+require './lib/passenger'
 
 describe Park do
     it "exsist" do
@@ -7,3 +9,36 @@ describe Park do
         expect(park).to be_an_instance_of(Park)
     end
 end
+
+describe "#entering" do
+    it 'tracks vehicles coming into park' do
+        park = Park.new("Denve Park", 18)
+        vehicle = Vehicle.new("2001", "Honda", "Civic")
+        park.entering(vehicle)
+
+        expect(park.vehicles_entered).to eq([vehicle])
+    end
+end
+
+describe "#list_passengers" do
+    it "lists all passengers that entered the park" do
+        park = Park.new("Denve Park", 18)
+        vehicle = Vehicle.new("2001", "Honda", "Civic")
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+        vehicle.add_passenger(charlie)
+        vehicle.add_passenger(taylor)
+        park.entering(vehicle)
+
+        expect(park.list_passengers).to eq("Charlie, Taylor")
+
+        jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+        vehicle.add_passenger(jude)
+
+        expect(park.list_passengers).to eq("Charlie, Taylor, Jude")
+    end
+end
+
+describe "#revenue" do
+    it ""
+
